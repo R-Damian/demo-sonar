d271e5eecd727db4cf581fc34c7fd86775499b65
Damian Reboredo <reboredodamian@hotmail.com>
2021-12-10 05:08:43 -0300

    asd

--- src/net/epidata/demo/java/Main.java
+++ src/net/epidata/demo/java/Main.java
@@ -3,11 +3,7 @@ package net.epidata.demo.java;
 public class Main {
 
 	public static void main(String[] args) {
-int target = -5;
-int num = 3;
 
-target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-target =+ num; // Noncompliant; target = 3
 		System.out.print("hola");
 		
 
