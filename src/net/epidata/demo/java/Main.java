4a2718a41ad87f5727185b3647551fe916aab353
Damian Reboredo <reboredodamian@hotmail.com>
2021-12-10 05:05:19 -0300

    asd

--- src/net/epidata/demo/java/Main.java
+++ src/net/epidata/demo/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
 int target = -5;
 int num = 3;
 
+
 target =- num;  // Noncompliant; target = -3. Is that really what's meant?
 target =+ num; // Noncompliant; target = 3
 		System.out.print("hola");
