548d8e1b7d909fcb8c458cf44f54e175c2fdd447
Damian Reboredo <reboredodamian@hotmail.com>
2022-09-10 02:08:05 -0300

    Update Main.java

--- src/net/epidata/demo/java/Main.java
+++ src/net/epidata/demo/java/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-/*
+
 int target = -5;
 int num = 3;
 
 target =- num;  // Noncompliant; target = -3. Is that really what's meant?
 target =+ num; // Noncompliant; target = 3
-	*/	
+		
 		System.out.print("hola");
 		
 
